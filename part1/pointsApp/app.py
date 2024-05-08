bedd606e6d96d077583f9df7f9fdb85b0a461eeb
duckduck <yaya.zhang@student.howest.be>
2024-05-08 12:19:54 +0200

    add front backend file

--- part1/pointsApp/app.py
+++ part1/pointsApp/app.py
@@ -40,4 +40,4 @@ if __name__ == '__main__':
         db.create_all()
 
     # Run the application
-    app.run(debug=True, host='127.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
